2e9178bcef209dffb1b1c960a6ae81330990074e
nx96 <nxnqzd@gmail.com>
2024-12-04 01:06:57 -0500

    NDQS: Set color by show

--- src/channel.py
+++ src/channel.py
@@ -1,11 +1,12 @@
 USERNAME = "SUERTE-TV"
 
 SHOWS = [
-     {"id":"sisomos",           "name": "Sí Somos"},
-     {"id":"sorbitodeopinion",  "name": "Sorbito de Opinión"},
-     {"id":"elremember",          "name": "El Remember!"},
-     {"id":"hueveando",         "name": "Hueveando"},
-     {"id":"siemprelomismo",   "name": "#SIEMPRELOMISMO"},
+    {"id": "sisomos",           "name": "Sí Somos",           "color": "#d0fc54"},
+    {"id": "sorbitodeopinion",  "name": "Sorbito de Opinión", "color": "blue"},
+    {"id": "elremember",        "name": "El Remember!",       "color": "#f00494"},
+    {"id": "hueveando",         "name": "Hueveando",          "color": "#fbcc0a"},
+    {"id": "siemprelomismo",    "name": "#SIEMPRELOMISMO",    "color": "#882ce4"},
 ]
 
 DEFAULT_SHOW = "Otros"
+DEFAULT_COLOR = "#ff6444"
