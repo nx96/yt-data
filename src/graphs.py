55a91c808ca363c0c16a2384f36ddf1b29c3aeb1
nx96 <nxnqzd@gmail.com>
2024-12-05 00:10:39 -0500

    NDQS: Add graphs

--- src/graphs.py
+++ src/graphs.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
-from src.channel import SHOWS, DEFAULT_COLOR
+from src.channel import DEFAULT_SHOW, SHOWS, DEFAULT_COLOR
+import time
+
+from src.utils import remove_special_chars
+
+def seconds_to_time(seconds):
+    return time.strftime("%HH:%MM:%SS", time.gmtime(seconds))
 
 FONT = {
     'family': 'Arial',
@@ -8,13 +14,16 @@ FONT = {
     'size': 16
 }
 
+PROGRAM_COLOR_MAP = {show["name"]: show["color"] for show in SHOWS}
+PROGRAM_COLOR_MAP["Otros"] = DEFAULT_COLOR
+
 def format_value(val):
     return "{:,}".format(int(val))
 
 def plot_text(df):
 
     fig = plt.figure(figsize=(4,2))
-    fig.patch.set_facecolor("#282424")
+    fig.patch.set_facecolor("black")
 
     # Agregar textos
     val = format_value(df['subscriber_count'][0])
@@ -29,26 +38,42 @@ def plot_text(df):
 
     plt.show()
 
+def plot_longest_video(df):
+    longest_video = df[~df['show_id'].isin([DEFAULT_SHOW])].sort_values(by='duration_seconds', ascending=False)
+    
+    fig = plt.figure(figsize=(4,4))
+    fig.patch.set_facecolor("black")
 
-def plot_show(df):
-    program_color_map = {show["name"]: show["color"] for show in SHOWS}
-    program_color_map["Otros"] = DEFAULT_COLOR
+    # Agregar textos
+    text = remove_special_chars(longest_video.iloc[0]["title"])
+    text = f'VIDEO: {text}'
+    plt.text(x=0.2, y=0.9, s=text, fontdict=FONT)
+    
+    text = f'LIKES: {longest_video.iloc[0]["like_count"]}'
+    plt.text(x=0.2, y=0.6, s=text, fontdict=FONT)
+    
+    text = f'DURACIÓN: {seconds_to_time(longest_video.iloc[0]["duration_seconds"])}'
+    plt.text(x=0.2, y=0.3, s=text, fontdict=FONT)
 
+    plt.axis('off')
+    plt.subplots_adjust(left=0.2, right=0.8, top=0.9, bottom=0.4)
 
+    plt.show()
+
+def plot_show_by_view_count(df):
     most_viewed_by_show = df.loc[df.groupby('show_id')['view_count'].idxmax()]
     for _, row in most_viewed_by_show.iterrows():
-        label = f"{row['show']} =>\t {row['title']} ({row['view_count']} vistas)"
+        label = f"{row['show']} =>\t {row['title']}"
         print(label)
 
     plt.figure(figsize=(10, 6))
     bars = plt.bar(
         most_viewed_by_show["show"], 
         most_viewed_by_show["view_count"], 
-        color=[program_color_map[show] for show in most_viewed_by_show["show"]]
+        color=[PROGRAM_COLOR_MAP[show] for show in most_viewed_by_show["show"]]
     )
 
     # Personalizar el gráfico
-    plt.title("Video más reproducido por programa", fontsize=15)
     plt.xlabel("Programa", fontsize=11)
     plt.ylabel("Reproducciones", fontsize=11)
     plt.xticks(rotation=45)
@@ -64,3 +89,62 @@ def plot_show(df):
     plt.tight_layout()
     plt.show()
 
+
+def plot_show_by_like_count(df):
+    most_liked_by_show = df.loc[df.groupby('show_id')['like_count'].idxmax()]
+    for _, row in most_liked_by_show.iterrows():
+        label = f"{row['show']} =>\t {row['title']}"
+        print(label)
+
+    plt.figure(figsize=(10, 6))
+    bars = plt.bar(
+        most_liked_by_show["show"], 
+        most_liked_by_show["like_count"], 
+        color=[PROGRAM_COLOR_MAP[show] for show in most_liked_by_show["show"]]
+    )
+
+    # Personalizar el gráfico
+    plt.xlabel("Programa", fontsize=11)
+    plt.ylabel("Reproducciones", fontsize=11)
+    plt.xticks(rotation=45)
+    plt.grid(axis="y", linestyle=":", alpha=0.7)
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(
+            x=bar.get_x() + bar.get_width() / 2,
+            y=height - 100,
+            s=format_value(height),
+            ha='center', va='bottom', fontsize=10
+        )
+    plt.tight_layout()
+    plt.show()
+
+
+def plot_show_by_comment_count(df):
+    most_comment_show = df.loc[df.groupby('show_id')['comment_count'].idxmax()]
+    for _, row in most_comment_show.iterrows():
+        label = f"{row['show']} =>\t {row['title']}"
+        print(label)
+
+    plt.figure(figsize=(10, 6))
+    bars = plt.bar(
+        most_comment_show["show"], 
+        most_comment_show["comment_count"], 
+        color=[PROGRAM_COLOR_MAP[show] for show in most_comment_show["show"]]
+    )
+
+    # Personalizar el gráfico
+    plt.xlabel("Programa", fontsize=11)
+    plt.ylabel("Reproducciones", fontsize=11)
+    plt.xticks(rotation=45)
+    plt.grid(axis="y", linestyle=":", alpha=0.7)
+    for bar in bars:
+        height = bar.get_height()
+        plt.text(
+            x=bar.get_x() + bar.get_width() / 2,
+            y=height - 100,
+            s=format_value(height),
+            ha='center', va='bottom', fontsize=10
+        )
+    plt.tight_layout()
+    plt.show()
