6d8d727b628f6ba4c19f71b169d0e042ae03509a
nx96 <nxnqzd@gmail.com>
2024-12-14 15:44:41 -0500

    NDQS: Add columns to df

--- src/data.py
+++ src/data.py
@@ -9,16 +9,14 @@ DIR_OUTPUT = 'outputs'
 
 def get_df_(username: str) -> Optional[str]:
     # call services
-    list_videos, list_videos_detail, view_count, subscriber_count = get_data_from_YT_channel(username)
+    list_videos, list_videos_detail, view_total_count, subscriber_count = get_data_from_YT_channel(username)
     # generate df
     df = pd.DataFrame(list_videos)
     df_detail = pd.DataFrame(list_videos_detail)
     # merge df
     df = pd.merge(df, df_detail, on='video_id', how='left')
-    df['view_total_count'] = view_count
+    df['view_total_count'] = view_total_count
     df['subscriber_count'] = subscriber_count
-    df["url"] = "https://www.youtube.com/watch?v=" + df["video_id"]
-    df["duration_seconds"] = df["duration"].apply(parse_duration_to_seconds)
     return df
 
 def generate_csv(username: str) -> Optional[str]:
@@ -42,12 +40,15 @@ def max_len_shows():
             max_len = len(show['id'])
     return max_len
  
-def add_column_show(username: str) -> Optional[str]:
+def add_columns_to_df(username: str) -> Optional[str]:
     path = f"./{DIR_OUTPUT}/data_{username}.csv"
     df = pd.read_csv(path)
     max_len = max_len_shows()
     df["show"] = ""
     df["show_id"] = ""
+    df['like_view'] = ((df["like_count"].astype(int)*100) / df["view_count"].astype(int) ).round(2)
+    df["url"] = "https://www.youtube.com/watch?v=" + df["video_id"]
+    df["duration_seconds"] = df["duration"].apply(parse_duration_to_seconds)
     for index in df.index:
         title = df.at[index, "title"]
         title = apply_all_format(title)
@@ -55,10 +56,10 @@ def add_column_show(username: str) -> Optional[str]:
         df.at[index, "show"] = DEFAULT_SHOW
         df.at[index, "show_id"] = DEFAULT_SHOW
         for show in SHOWS:
-            if title.find(show['id']) != -1:
+            if title.find(show['id']) != -1 and df.at[index, "duration_seconds"] > 1800:
                 df.at[index, "show"] = show['name']
                 df.at[index, "show_id"] = show['id']
                 break
-    df = df.loc[:, ['video_id', 'show', 'show_id','title', 'view_count', 'like_count', 'favorite_count', 'comment_count', 'duration', 'duration_seconds', 'published_at', 'url', 'view_total_count', 'subscriber_count']]
+    df = df.loc[:, ['video_id', 'show', 'show_id','title', 'view_count', 'like_count', 'like_view', 'favorite_count', 'comment_count', 'duration', 'duration_seconds', 'published_at', 'url', 'view_total_count', 'subscriber_count']]
     df.to_csv(path, index=False)
     return df
