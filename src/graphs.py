1cd059bb4687fc284877cbf78db3a5a9ab3ad400
nx96 <nxnqzd@gmail.com>
2024-12-15 17:42:56 -0500

    NDQS: Add plots and format code

--- src/graphs.py
+++ src/graphs.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 from src.channel import DEFAULT_SHOW, SHOWS, DEFAULT_COLOR
 import pandas as pd
-import time
-
+from typing import List, Optional
 from src.utils import delete_emojis
 
-def seconds_to_time(seconds):
-    return time.strftime("%HH:%MM:%SS", time.gmtime(seconds))
-
+def seconds_to_time(seconds: int):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    secs = seconds % 60
+    return f"{hours:02}H:{minutes:02}M:{secs:02}S"
+    
 FONT = {
     'family': 'Arial',
     'color': DEFAULT_COLOR,
@@ -27,7 +30,7 @@ PROGRAM_COLOR_MAP["Otros"] = DEFAULT_COLOR
 def format_value(val):
     return "{:,}".format(int(val))
 
-def plot_text(df):
+def plot_text(df: pd.DataFrame):
 
     fig = plt.figure(figsize=(4,2))
     fig.patch.set_facecolor("black")
@@ -45,7 +48,7 @@ def plot_text(df):
 
     plt.show()
 
-def plot_longest_video(df):
+def plot_longest_video(df: pd.DataFrame):
     longest_video = df[~df['show_id'].isin([DEFAULT_SHOW])].sort_values(by='duration_seconds', ascending=False)
     
     fig = plt.figure(figsize=(4,4))
@@ -68,7 +71,7 @@ def plot_longest_video(df):
     plt.show()
 
 
-def plot_base(df: pd.DataFrame, colum: str, ylabel : str):
+def plot_base(df: pd.DataFrame, colum: str, ylabel : str, limit_height: Optional[bool] = True, print_legends: Optional[bool] = True, format_time: Optional[bool] = False):
     plt.figure(figsize=(10, 6))
     bars = plt.bar(
         df["show"], 
@@ -84,26 +87,51 @@ def plot_base(df: pd.DataFrame, colum: str, ylabel : str):
         height = bar.get_height()
         plt.text(
             x=bar.get_x() + bar.get_width() / 2,
-            y=height - 100,
-            s=format_value(height),
+            y= height if not limit_height else height - 100,
+            s= format_value(height) if not format_time else seconds_to_time(height),
             ha='center', va='bottom', fontsize=10
         )
 
-    legend_elements = []
-    for _, row in df.iterrows():
-        color = PROGRAM_COLOR_MAP[row["show"]]
-        label = f"{delete_emojis(row['title'])}"
-        legend_elements.append(plt.Rectangle((0, 0), 1, 1, color=color, label=label))
-    
-    plt.legend(
-        handles=legend_elements,
-        loc='center',
-        fontsize=9,
-        title_fontsize=10,
-        frameon=True,
-        bbox_to_anchor=(0.5, 1.15),
-        ncol=2
+    if print_legends:
+        legend_elements = []
+        for _, row in df.iterrows():
+            color = PROGRAM_COLOR_MAP[row["show"]]
+            label = f"{delete_emojis(row['title'])}"
+            legend_elements.append(plt.Rectangle((0, 0), 1, 1, color=color, label=label))
+        
+        plt.legend(
+            handles=legend_elements,
+            loc='center',
+            fontsize=9,
+            title_fontsize=10,
+            frameon=True,
+            bbox_to_anchor=(0.5, 1.15),
+            ncol=2
+        )
+
+    plt.tight_layout()
+    plt.show()
+
+def plot_evolution(df: pd.DataFrame, color: str, colum: str, ylabel: str, format_legends: Optional[bool] = False):
+    df['published_at'] = pd.to_datetime(df['published_at'])
+    plt.figure(figsize=(10, 6))
+    plt.plot(
+        df['published_at'], 
+        df[colum], 
+        color=color, 
+        linewidth=2, 
+        marker='.', 
+        markerfacecolor='black'
     )
+
+    if format_legends:
+        formatter = FuncFormatter(lambda x, _: seconds_to_time(seconds=int(x)))
+        plt.gca().yaxis.set_major_formatter(formatter)
+
+    plt.xlabel("Fecha", fontsize=11)
+    plt.ylabel(ylabel, fontsize=11)
+    plt.xticks(rotation=45)
+    plt.grid(axis="y", linestyle=":", alpha=0.8)
 
     plt.tight_layout()
     plt.show()
@@ -123,26 +151,6 @@ def plot_show_by_comment_count(df: pd.DataFrame):
 
     plot_base(df=most_comment_show, colum="comment_count", ylabel="Comentarios")
 
-def plot_evolution(df: pd.DataFrame, color: str, colum: str):
-    df['published_at'] = pd.to_datetime(df['published_at'])
-    plt.figure(figsize=(10, 6))
-    plt.plot(
-        df['published_at'], 
-        df[colum], 
-        color=color, 
-        linewidth=2, 
-        marker='.', 
-        markerfacecolor='black'
-    )
-
-    plt.xlabel("Fecha", fontsize=11)
-    plt.ylabel("Popularidad", fontsize=11)
-    plt.xticks(rotation=45)
-    plt.grid(axis="y", linestyle=":", alpha=0.8)
-
-    plt.tight_layout()
-    plt.show()
-
 def plot_evolution_by_view(df: pd.DataFrame, program : str):
     evolution_program = df[df['show'] == program].sort_values(by='published_at')
     color=PROGRAM_COLOR_MAP[program]
@@ -153,10 +161,24 @@ def plot_evolution_by_like(df: pd.DataFrame, program : str):
     evolution_program = df[df['show'] == program].sort_values(by='published_at')
     color=PROGRAM_COLOR_MAP[program]
 
-    plot_evolution(df=evolution_program, color=color, colum="like_count")
+    plot_evolution(df=evolution_program, color=color, colum="like_count", ylabel='Likes')
 
 def plot_evolution_by_popularity(df: pd.DataFrame, program : str):
     evolution_program = df[df['show'] == program].sort_values(by='published_at')
     color=PROGRAM_COLOR_MAP[program]
 
-    plot_evolution(df=evolution_program, color=color, colum="like_view")
+    plot_evolution(df=evolution_program, color=color, colum="like_view", ylabel='Popularidad')
+
+def plot_evolution_by_duration(df: pd.DataFrame, program : str):
+    evolution_program = df[df['show'] == program].sort_values(by="published_at")
+    color=PROGRAM_COLOR_MAP[program]
+
+    plot_evolution(df=evolution_program, color=color, colum="duration_seconds", ylabel='Tiempo', format_legends=True)
+
+def plot_chapters_by_show(df: pd.DataFrame):
+    df_count = df[~df['show_id'].isin([DEFAULT_SHOW])].groupby(['show']).agg({'title': 'count'}).sort_values(by='title', ascending=True).reset_index()
+    plot_base(df=df_count, colum="title", ylabel="Capítulos", limit_height=False, print_legends=False)
+
+def plot_duration_by_show(df: pd.DataFrame):
+    df_duration = df[~df['show_id'].isin([DEFAULT_SHOW])].groupby(['show']).agg({'duration_seconds': 'sum'}).sort_values(by='duration_seconds', ascending=True).reset_index()
+    plot_base(df=df_duration, colum="duration_seconds", ylabel="Duración", limit_height=True, print_legends=False, format_time=True)
